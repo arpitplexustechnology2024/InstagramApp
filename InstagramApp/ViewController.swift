42de8e067759876c0ecdeb8bd3fa76539acc72c1
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-16 15:23:16 -0700

    Initial Commit

---
+++ InstagramApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by Arpit iOS Dev. on 16/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
